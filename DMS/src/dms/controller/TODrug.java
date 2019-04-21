1fe0a02d3b601d1a8ca61ecaac61c30edb4cbfff
willbouch <williambouchardpei@gmail.com>
2019-04-21 19:09:12 -0400

    Added 2 TObjects

---
+++ DMS/src/dms/controller/TODrug.java
@@ -0,0 +1,204 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
+
+package dms.controller;
+
+// line 7 "../../DMS_TObjects.ump"
+public class TODrug
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TODrug Attributes
+  private String name;
+  private double price;
+  private double concentration;
+  private String unit;
+  private int inHandQuantity;
+  private int orderedQuantity;
+  private int minQuantity;
+  private int id;
+
+  //TODrug Associations
+  private TOInventory tOInventory;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TODrug(String aName, double aPrice, double aConcentration, String aUnit, int aInHandQuantity, int aOrderedQuantity, int aMinQuantity, int aId, TOInventory aTOInventory)
+  {
+    name = aName;
+    price = aPrice;
+    concentration = aConcentration;
+    unit = aUnit;
+    inHandQuantity = aInHandQuantity;
+    orderedQuantity = aOrderedQuantity;
+    minQuantity = aMinQuantity;
+    id = aId;
+    boolean didAddTOInventory = setTOInventory(aTOInventory);
+    if (!didAddTOInventory)
+    {
+      throw new RuntimeException("Unable to create tODrug due to tOInventory");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setPrice(double aPrice)
+  {
+    boolean wasSet = false;
+    price = aPrice;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setConcentration(double aConcentration)
+  {
+    boolean wasSet = false;
+    concentration = aConcentration;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setUnit(String aUnit)
+  {
+    boolean wasSet = false;
+    unit = aUnit;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setInHandQuantity(int aInHandQuantity)
+  {
+    boolean wasSet = false;
+    inHandQuantity = aInHandQuantity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setOrderedQuantity(int aOrderedQuantity)
+  {
+    boolean wasSet = false;
+    orderedQuantity = aOrderedQuantity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setMinQuantity(int aMinQuantity)
+  {
+    boolean wasSet = false;
+    minQuantity = aMinQuantity;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setId(int aId)
+  {
+    boolean wasSet = false;
+    id = aId;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public double getPrice()
+  {
+    return price;
+  }
+
+  public double getConcentration()
+  {
+    return concentration;
+  }
+
+  public String getUnit()
+  {
+    return unit;
+  }
+
+  public int getInHandQuantity()
+  {
+    return inHandQuantity;
+  }
+
+  public int getOrderedQuantity()
+  {
+    return orderedQuantity;
+  }
+
+  public int getMinQuantity()
+  {
+    return minQuantity;
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+  /* Code from template association_GetOne */
+  public TOInventory getTOInventory()
+  {
+    return tOInventory;
+  }
+  /* Code from template association_SetOneToMany */
+  public boolean setTOInventory(TOInventory aTOInventory)
+  {
+    boolean wasSet = false;
+    if (aTOInventory == null)
+    {
+      return wasSet;
+    }
+
+    TOInventory existingTOInventory = tOInventory;
+    tOInventory = aTOInventory;
+    if (existingTOInventory != null && !existingTOInventory.equals(aTOInventory))
+    {
+      existingTOInventory.removeTODrug(this);
+    }
+    tOInventory.addTODrug(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    TOInventory placeholderTOInventory = tOInventory;
+    this.tOInventory = null;
+    if(placeholderTOInventory != null)
+    {
+      placeholderTOInventory.removeTODrug(this);
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "price" + ":" + getPrice()+ "," +
+            "concentration" + ":" + getConcentration()+ "," +
+            "unit" + ":" + getUnit()+ "," +
+            "inHandQuantity" + ":" + getInHandQuantity()+ "," +
+            "orderedQuantity" + ":" + getOrderedQuantity()+ "," +
+            "minQuantity" + ":" + getMinQuantity()+ "," +
+            "id" + ":" + getId()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "tOInventory = "+(getTOInventory()!=null?Integer.toHexString(System.identityHashCode(getTOInventory())):"null");
+  }
+}
---
+++ DMS/src/dms/controller/TOInventory.java
@@ -0,0 +1,142 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
+
+package dms.controller;
+import java.util.*;
+
+// line 3 "../../DMS_TObjects.ump"
+public class TOInventory
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TOInventory Associations
+  private List<TODrug> tODrugs;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TOInventory()
+  {
+    tODrugs = new ArrayList<TODrug>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+  /* Code from template association_GetMany */
+  public TODrug getTODrug(int index)
+  {
+    TODrug aTODrug = tODrugs.get(index);
+    return aTODrug;
+  }
+
+  public List<TODrug> getTODrugs()
+  {
+    List<TODrug> newTODrugs = Collections.unmodifiableList(tODrugs);
+    return newTODrugs;
+  }
+
+  public int numberOfTODrugs()
+  {
+    int number = tODrugs.size();
+    return number;
+  }
+
+  public boolean hasTODrugs()
+  {
+    boolean has = tODrugs.size() > 0;
+    return has;
+  }
+
+  public int indexOfTODrug(TODrug aTODrug)
+  {
+    int index = tODrugs.indexOf(aTODrug);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfTODrugs()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public TODrug addTODrug(String aName, double aPrice, double aConcentration, String aUnit, int aInHandQuantity, int aOrderedQuantity, int aMinQuantity, int aId)
+  {
+    return new TODrug(aName, aPrice, aConcentration, aUnit, aInHandQuantity, aOrderedQuantity, aMinQuantity, aId, this);
+  }
+
+  public boolean addTODrug(TODrug aTODrug)
+  {
+    boolean wasAdded = false;
+    if (tODrugs.contains(aTODrug)) { return false; }
+    TOInventory existingTOInventory = aTODrug.getTOInventory();
+    boolean isNewTOInventory = existingTOInventory != null && !this.equals(existingTOInventory);
+    if (isNewTOInventory)
+    {
+      aTODrug.setTOInventory(this);
+    }
+    else
+    {
+      tODrugs.add(aTODrug);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeTODrug(TODrug aTODrug)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aTODrug, as it must always have a tOInventory
+    if (!this.equals(aTODrug.getTOInventory()))
+    {
+      tODrugs.remove(aTODrug);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addTODrugAt(TODrug aTODrug, int index)
+  {  
+    boolean wasAdded = false;
+    if(addTODrug(aTODrug))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTODrugs()) { index = numberOfTODrugs() - 1; }
+      tODrugs.remove(aTODrug);
+      tODrugs.add(index, aTODrug);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveTODrugAt(TODrug aTODrug, int index)
+  {
+    boolean wasAdded = false;
+    if(tODrugs.contains(aTODrug))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTODrugs()) { index = numberOfTODrugs() - 1; }
+      tODrugs.remove(aTODrug);
+      tODrugs.add(index, aTODrug);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addTODrugAt(aTODrug, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    for(int i=tODrugs.size(); i > 0; i--)
+    {
+      TODrug aTODrug = tODrugs.get(i - 1);
+      aTODrug.delete();
+    }
+  }
+
+}

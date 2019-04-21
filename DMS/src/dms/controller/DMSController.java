deba2fc2fb108ffec77baa77930f11f43be11b5c
willbouch <williambouchardpei@gmail.com>
2019-04-20 21:29:08 -0400

    coded updateDrug()

--- DMS/src/dms/controller/DMSController.java
+++ DMS/src/dms/controller/DMSController.java
@@ -3,6 +3,7 @@ package dms.controller;
 import dms.application.DMSApplication;
 import dms.model.Cashier;
 import dms.model.DMS;
+import dms.model.Drug;
 import dms.model.Inventory;
 import dms.model.UserRole;
 import dms.persistence.DMSPersistence;
@@ -20,19 +21,14 @@ public class DMSController {
 		//Change the "structure" of the name
 		name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
 		
-		//Find the inventory according to first letter
-		char firstLetter = name.charAt(0);
-		int index = 0;
-		for(Inventory inv : dms.getInventories()) {
-			if(firstLetter == inv.getFirstLetter()) {
-				break;
-			}
-			index++;
+		Inventory inventory = dms.findInventory(name);
+		if(inventory == null) {
+			throw new InvalidInputException("L'inventaire n'existe pas.");
 		}
 		
 		//We add the drug to the inventory we found and we save the file
 		try {
-			dms.getInventory(index).addDrug(name, price, concentration, unit, inHandQuantity, minQuantity);
+			inventory.addDrug(name, price, concentration, unit, inHandQuantity, minQuantity);
 			DMSPersistence.save(dms);
 		}
 		catch(RuntimeException e) {
@@ -41,4 +37,32 @@ public class DMSController {
 
 	}
 
+	public static void updateDrug(String name, int id, int newInHandQuantity, int newMinQuantity, double newPrice) throws InvalidInputException {
+		UserRole currentUserRole = DMSApplication.getCurrentUserRole();
+		DMS dms = DMSApplication.getDMS();
+		
+		if(currentUserRole instanceof Cashier) {
+			throw new InvalidInputException("Vous n'avez pas les droits nécessaires pour cette opération.");
+		}
+		
+		Inventory inventory = dms.findInventory(name);
+		if(inventory == null) {
+			throw new InvalidInputException("L'inventaire n'existe pas.");
+		}
+		
+		Drug drug = inventory.findDrug(id);
+		if(drug == null) {
+			throw new InvalidInputException("Le médicament n'existe pas.");
+		}
+		
+		try {
+			drug.setInHandQuantity(newInHandQuantity);
+			drug.setMinQuantity(newMinQuantity);
+			drug.setPrice(newPrice);
+			DMSPersistence.save(dms);
+		}
+		catch(RuntimeException e) {
+			throw new InvalidInputException(e.getMessage());
+		}
+	}
 }

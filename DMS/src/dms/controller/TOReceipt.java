9e62986899224ecc823f348927f50003b5a9584c
willbouch <williambouchardpei@gmail.com>
2019-05-27 14:24:39 -0400

    added TO for receipt and scannedItem

---
+++ DMS/src/dms/controller/TOReceipt.java
@@ -0,0 +1,142 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
+
+package dms.controller;
+import java.util.*;
+
+// line 18 "../../DMS_TObjects.ump"
+public class TOReceipt
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TOReceipt Associations
+  private List<TOScannedItem> tOScannedItems;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TOReceipt()
+  {
+    tOScannedItems = new ArrayList<TOScannedItem>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+  /* Code from template association_GetMany */
+  public TOScannedItem getTOScannedItem(int index)
+  {
+    TOScannedItem aTOScannedItem = tOScannedItems.get(index);
+    return aTOScannedItem;
+  }
+
+  public List<TOScannedItem> getTOScannedItems()
+  {
+    List<TOScannedItem> newTOScannedItems = Collections.unmodifiableList(tOScannedItems);
+    return newTOScannedItems;
+  }
+
+  public int numberOfTOScannedItems()
+  {
+    int number = tOScannedItems.size();
+    return number;
+  }
+
+  public boolean hasTOScannedItems()
+  {
+    boolean has = tOScannedItems.size() > 0;
+    return has;
+  }
+
+  public int indexOfTOScannedItem(TOScannedItem aTOScannedItem)
+  {
+    int index = tOScannedItems.indexOf(aTOScannedItem);
+    return index;
+  }
+  /* Code from template association_MinimumNumberOfMethod */
+  public static int minimumNumberOfTOScannedItems()
+  {
+    return 0;
+  }
+  /* Code from template association_AddManyToOne */
+  public TOScannedItem addTOScannedItem(String aName, String aCode, double aPrice)
+  {
+    return new TOScannedItem(aName, aCode, aPrice, this);
+  }
+
+  public boolean addTOScannedItem(TOScannedItem aTOScannedItem)
+  {
+    boolean wasAdded = false;
+    if (tOScannedItems.contains(aTOScannedItem)) { return false; }
+    TOReceipt existingTOReceipt = aTOScannedItem.getTOReceipt();
+    boolean isNewTOReceipt = existingTOReceipt != null && !this.equals(existingTOReceipt);
+    if (isNewTOReceipt)
+    {
+      aTOScannedItem.setTOReceipt(this);
+    }
+    else
+    {
+      tOScannedItems.add(aTOScannedItem);
+    }
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeTOScannedItem(TOScannedItem aTOScannedItem)
+  {
+    boolean wasRemoved = false;
+    //Unable to remove aTOScannedItem, as it must always have a tOReceipt
+    if (!this.equals(aTOScannedItem.getTOReceipt()))
+    {
+      tOScannedItems.remove(aTOScannedItem);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+  /* Code from template association_AddIndexControlFunctions */
+  public boolean addTOScannedItemAt(TOScannedItem aTOScannedItem, int index)
+  {  
+    boolean wasAdded = false;
+    if(addTOScannedItem(aTOScannedItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTOScannedItems()) { index = numberOfTOScannedItems() - 1; }
+      tOScannedItems.remove(aTOScannedItem);
+      tOScannedItems.add(index, aTOScannedItem);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveTOScannedItemAt(TOScannedItem aTOScannedItem, int index)
+  {
+    boolean wasAdded = false;
+    if(tOScannedItems.contains(aTOScannedItem))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTOScannedItems()) { index = numberOfTOScannedItems() - 1; }
+      tOScannedItems.remove(aTOScannedItem);
+      tOScannedItems.add(index, aTOScannedItem);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addTOScannedItemAt(aTOScannedItem, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    for(int i=tOScannedItems.size(); i > 0; i--)
+    {
+      TOScannedItem aTOScannedItem = tOScannedItems.get(i - 1);
+      aTOScannedItem.delete();
+    }
+  }
+
+}
---
+++ DMS/src/dms/controller/TOScannedItem.java
@@ -0,0 +1,124 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.29.0.4181.a593105a9 modeling language!*/
+
+package dms.controller;
+
+// line 22 "../../DMS_TObjects.ump"
+public class TOScannedItem
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TOScannedItem Attributes
+  private String name;
+  private String code;
+  private double price;
+
+  //TOScannedItem Associations
+  private TOReceipt tOReceipt;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TOScannedItem(String aName, String aCode, double aPrice, TOReceipt aTOReceipt)
+  {
+    name = aName;
+    code = aCode;
+    price = aPrice;
+    boolean didAddTOReceipt = setTOReceipt(aTOReceipt);
+    if (!didAddTOReceipt)
+    {
+      throw new RuntimeException("Unable to create tOScannedItem due to tOReceipt");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setName(String aName)
+  {
+    boolean wasSet = false;
+    name = aName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setCode(String aCode)
+  {
+    boolean wasSet = false;
+    code = aCode;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setPrice(double aPrice)
+  {
+    boolean wasSet = false;
+    price = aPrice;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public String getCode()
+  {
+    return code;
+  }
+
+  public double getPrice()
+  {
+    return price;
+  }
+  /* Code from template association_GetOne */
+  public TOReceipt getTOReceipt()
+  {
+    return tOReceipt;
+  }
+  /* Code from template association_SetOneToMany */
+  public boolean setTOReceipt(TOReceipt aTOReceipt)
+  {
+    boolean wasSet = false;
+    if (aTOReceipt == null)
+    {
+      return wasSet;
+    }
+
+    TOReceipt existingTOReceipt = tOReceipt;
+    tOReceipt = aTOReceipt;
+    if (existingTOReceipt != null && !existingTOReceipt.equals(aTOReceipt))
+    {
+      existingTOReceipt.removeTOScannedItem(this);
+    }
+    tOReceipt.addTOScannedItem(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    TOReceipt placeholderTOReceipt = tOReceipt;
+    this.tOReceipt = null;
+    if(placeholderTOReceipt != null)
+    {
+      placeholderTOReceipt.removeTOScannedItem(this);
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "name" + ":" + getName()+ "," +
+            "code" + ":" + getCode()+ "," +
+            "price" + ":" + getPrice()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "tOReceipt = "+(getTOReceipt()!=null?Integer.toHexString(System.identityHashCode(getTOReceipt())):"null");
+  }
+}
